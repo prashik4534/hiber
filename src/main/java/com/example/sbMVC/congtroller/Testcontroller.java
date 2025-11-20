918a0d31542a2e223b604e87cd10fbbff6b9b8eb
prashik4534 <prashikshirsat81@gmail.com>
2025-11-20 19:03:01 +0530

    first commit

---
+++ src/main/java/com/example/sbMVC/congtroller/Testcontroller.java
@@ -0,0 +1,28 @@
+package com.example.sbMVC.congtroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class Testcontroller {
+	
+	@RequestMapping("/")
+	public String login() {
+		
+		return "login";
+	}
+		
+	@RequestMapping("login-user")
+	public String loginuser(@RequestParam String username, @RequestParam String password) {
+		
+		System.out.println(username);
+		System.out.println(password);
+		return "login";
+	
+		
+	}
+	
+
+}
---
+++ src/main/webapp/pages/login.jsp
@@ -0,0 +1,27 @@
+
+<HTML>
+<head>
+<title> Login </title>
+</head>
+<body>
+
+ <form action="/login-user"›
+
+
+   <label>Username :</label> <input type="text" name="username"><br>
+   
+        
+    <br>
+
+    <label>Password :</label> <input type="text" name="password"><br><br>
+    
+    <input type="submit" name="login"><br>
+    
+    
+    
+    <br>
+   </form>
+   </body>
+    
+</HTML>
+
